339cbea1848ee5ba6a4a3f876fbf3c476ab2d128
robbdouglas <robert.nimmrich@icloud.com>
2023-09-07 07:06:45 +0200

    edit html and js to stop refreshing and send empty dheckboxes

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 function addTodo() {
-  todolist.innerHTML += `<li class="mdl-list__item">
-    <span class="mdl-list__item-primary-content">
-      <i class="fa-solid fa-arrow-right"></i>
-      ${todofield.value}
-    </span>
-    <span class="mdl-list__item-secondary-action">
-      <label
-        class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect"
-        for="list-checkbox-1"
-      >
-        <input
-          type="checkbox"
-          id="list-checkbox-1"
-          class="mdl-checkbox__input"
-          checked
-        />
-      </label>
-    </span>
-  </li>`;
+  var todofield = document.getElementById("todofield");
+  var todolist = document.getElementById("todolist");
+
+  var todoText = todofield.value.trim(); // Leerzeichen am Anfang und Ende entfernen
+
+  if (todoText !== "") {
+    // Überprüfen, ob der Text nicht leer ist
+    var listItem = document.createElement("li");
+    listItem.className = "mdl-list__item";
+    listItem.innerHTML = `
+        <span class="mdl-list__item-primary-content">
+          <i class="fa-solid fa-arrow-right"></i>
+          ${todoText}
+        </span>
+        <span class="mdl-list__item-secondary-action">
+          <label
+            class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect"
+          >
+            <input
+              type="checkbox"
+              class="mdl-checkbox__input"
+            />
+          </label>
+        </span>
+      `;
+
+    todolist.appendChild(listItem); // Füge das Listenelement zur Liste hinzu
+
+    todofield.value = ""; // Setze das Eingabefeld zurück
+  }
 }
